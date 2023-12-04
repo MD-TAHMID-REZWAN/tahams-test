812aaa6e396ca25ba95b077ca10914af5836b440
MD-TAHMID-REZWAN <tahmidnishad@gmail.com>
2023-12-04 16:55:33 +0600

    hi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AdminModule } from './Admin/Modules/adminModule.module';
 import { CustomerModule } from './Customer/Modules/customerModule.module';
 import { GlobalModule } from './Global/Modules/global.module';
 import { EmployeeModule } from './Employee/Modules/employeeModule.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -13,17 +15,21 @@ import { EmployeeModule } from './Employee/Modules/employeeModule.module';
     EmployeeModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: 'monorail.proxy.rlwy.net', //localhost
+      port: 45372, //5432
       username: 'postgres',
-      password: 'root',
-      database: 'Tahams',
+      password: 'AAG-Dcg5GE3g5dCE5D6e6Ga2bdBfD34g', //root
+      database: 'railway', //tahams
       autoLoadEntities: true,
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([]),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../public'), // added ../ to get one folder back
+      serveRoot: '/public/', //last slash was important
+    }),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
